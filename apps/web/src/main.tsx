18b16d631565b66b1ef66bbdc9b2930e6334a845
cutiechi <superchijinpeng@gmail.com>
2026-08-06 10:46:52 +0800

    feat(web): wrap app in ReadingSettingsProvider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "@/components/theme-provider"
+import { ReadingSettingsProvider } from "@/components/reading-settings"
 import { App } from "./App"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ReadingSettingsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReadingSettingsProvider>
     </ThemeProvider>
   </StrictMode>
 )
